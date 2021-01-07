e89cc86ebe581e10bf34e1a5ffa8ea9b2a05cb86
carloslesmes <carloslesmes@gmail.com>
2021-01-07 08:12:24 -0500

    mejora

--- Pluto2.jl
+++ Pluto2.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.12.17
+# v0.12.18
 
 using Markdown
 using InteractiveUtils
@@ -16,11 +16,16 @@ end
 # ╔═╡ 592af9c8-3d53-11eb-2948-8bba2d583f1e
 begin
 using Dates
+using PlutoUI	
 @bind fecha DateField(default=today())
 end
 
 # ╔═╡ 1b4aaf82-0675-11eb-1b20-07ace1dee0e8
-using Distributions, Plots, StatsPlots, PlutoUI
+begin
+using Distributions
+using Plots
+using StatsPlots
+end
 
 # ╔═╡ 482b887c-3d56-11eb-1be0-97c811ec5535
 using DataFrames, RDatasets
@@ -47,7 +52,7 @@ md"""
 
 Este notebook se actualiza a medida que se cambia un valor. Permite utilizar Markdown y escribir ecuaciones con LaTeX:
 
-$$\int_{0}^{1}\frac{1}{√(2π)\sigma}e^{-\left(\frac{x-μ}{σ}\right)^{2}}\,dx$$
+$$\int_{0}^{1}\frac{1}{\sigma√(2π)}e^{-\left(\frac{x-μ}{σ}\right)^{2}}\,dx$$
 
 Se pueden agregar sliders para las variables dentro y fuera de markdown.
 
@@ -105,25 +110,25 @@ iris = dataset("datasets","iris")
 describe(iris)
 
 # ╔═╡ Cell order:
-# ╠═592af9c8-3d53-11eb-2948-8bba2d583f1e
+# ╟─592af9c8-3d53-11eb-2948-8bba2d583f1e
 # ╠═9a00d67e-1081-11eb-25f8-f5a615d4d182
 # ╠═a3f6e1ec-1080-11eb-1def-393f6a9866d6
-# ╠═9b292610-1080-11eb-05a5-bf585221db9c
-# ╠═abb3ad16-1080-11eb-281f-fdb083207e96
+# ╟─9b292610-1080-11eb-05a5-bf585221db9c
+# ╟─abb3ad16-1080-11eb-281f-fdb083207e96
 # ╠═f82c9daa-0677-11eb-07eb-435641000188
-# ╠═76be5b06-0676-11eb-143a-a56a1afdeb1c
-# ╠═1b4aaf82-0675-11eb-1b20-07ace1dee0e8
-# ╠═168cc380-0676-11eb-2d24-5fed2d808828
-# ╠═61e076fc-0675-11eb-1f3b-a9b03c598888
-# ╠═b3d3204e-1082-11eb-39b8-e1c1156d415f
-# ╠═bae8cb88-1083-11eb-021a-b53f570491fa
-# ╠═d752a3c6-1082-11eb-2fa6-f1166143b001
-# ╠═f5c4beb6-1082-11eb-2ccb-8ddf0253824c
-# ╠═0fe53530-1096-11eb-0270-af7a2092278c
-# ╠═a4604ff0-10a9-11eb-2c50-6909307f5c73
-# ╠═776f61cc-1084-11eb-2f40-d7d6d514ce6d
+# ╟─76be5b06-0676-11eb-143a-a56a1afdeb1c
+# ╟─1b4aaf82-0675-11eb-1b20-07ace1dee0e8
+# ╟─168cc380-0676-11eb-2d24-5fed2d808828
+# ╟─61e076fc-0675-11eb-1f3b-a9b03c598888
+# ╟─b3d3204e-1082-11eb-39b8-e1c1156d415f
+# ╟─bae8cb88-1083-11eb-021a-b53f570491fa
+# ╟─d752a3c6-1082-11eb-2fa6-f1166143b001
+# ╟─f5c4beb6-1082-11eb-2ccb-8ddf0253824c
+# ╟─0fe53530-1096-11eb-0270-af7a2092278c
+# ╟─a4604ff0-10a9-11eb-2c50-6909307f5c73
+# ╟─776f61cc-1084-11eb-2f40-d7d6d514ce6d
 # ╠═46fea92e-3d54-11eb-3f0d-33c2ef2ad1cb
-# ╠═a5944fac-3d54-11eb-3f33-b5375a6ce87a
-# ╠═482b887c-3d56-11eb-1be0-97c811ec5535
-# ╠═7d5f8d6a-3d56-11eb-1569-77f200f14c74
-# ╠═f4bd5982-3d56-11eb-30cd-5f254d209514
+# ╟─a5944fac-3d54-11eb-3f33-b5375a6ce87a
+# ╟─482b887c-3d56-11eb-1be0-97c811ec5535
+# ╟─7d5f8d6a-3d56-11eb-1569-77f200f14c74
+# ╟─f4bd5982-3d56-11eb-30cd-5f254d209514
